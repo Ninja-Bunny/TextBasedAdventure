b26f88cf89351744e8104d1b7240f1ee04c0e509
Melli <melaniemueller.21@web.de>
2020-11-16 19:23:06 +0100

    tried to add input stream

--- Stage.hpp
+++ Stage.hpp
@@ -9,17 +9,28 @@ using namespace std;
 class Stage
 {
     public: 
-        Stage(string id, string name, string description); 
+        Stage(string id, string name, string description, string question, string choice0, string choice1, string choice2, string choice3, string choice4); 
 
     private: 
-        string id; 
-        string name; 
-        string description; 
-        string choices[5]; 
+        string id;  //stageID
+        string name; //stageName
+        string description; //shortTest
+        string question; //Question to Answer
+        string choices[5]; //choices
 }; 
 
-Stage::Stage(string id, string name, string description)
+Stage::Stage(string id, string name, string description, string question, string choice0, string choice1, string choice2, string choice3, string choice4)
 {
+    //Konstruktor
+ this->id = id; 
+ this->name = name; 
+ this->description = description; 
+ this-> question = question; 
+ choices[0] = choice0; 
+ choices[1] = choice1;
+ choices[2] = choice2;
+ choices[3] = choice3;
+ choices[4] = choice4;
 
 }
 
