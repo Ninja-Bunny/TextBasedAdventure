2c10408c042e7ccc7adc7107385328e6b3f117a0
Melli <melaniemueller.21@web.de>
2020-11-16 17:05:22 +0100

    added hpp

--- Game.hpp
+++ Game.hpp
@@ -1,5 +1,5 @@
-#ifndef GAME_H
-#define GAME_H
+#ifndef GAME_HPP
+#define GAME_HPP
 #endif
 
 #include <iostream>
@@ -17,22 +17,15 @@ class Game
     Game(); //Constructor 
     ~Game(); //Destructor -> brauchen wir den? 
     void run(); 
-    void createRoom(string id, string name, string description, string north, string east, string south, string west); 
-    void roomSetUp(); 
-    map<string, Room*> rooms; 
 
     private: 
-    Room* currentRoom; 
-    string endRoom; 
 
 
 }; 
 
 Game::Game() //define Constructor 
 {
-    currentRoom = nullptr; 
-    string endRoom = ""; 
-    roomSetUp(); 
+
 }
 
 void Game::run() //define void run -> should run/start the game 
@@ -40,50 +33,3 @@ void Game::run() //define void run -> should run/start the game
     //schleife in der das Spiel läuft 
 }
 
-void Game::roomSetUp()
-{
-    string file = "rooms.txt"; 
-    ifstream input (file); 
-    
-    string buffer, id, name, description, north, east, south, west; 
-
-    while(input >> buffer)
-    {
-        if(buffer == "BEGIN_ROOM")
-        {
-            input >> id; 
-            input.ignore(); 
-            getline(input, name); 
-            getline(input, description); 
-
-            input >> buffer; //North
-            input >> north; 
-
-            input >> buffer; //East
-            input >> east; 
-
-            input >> buffer; //South
-            input >> south; 
-
-            input >> buffer; //West
-            input >> west; 
-
-            createRoom(id, name, description, north, east, south, west); 
-        }
-        else if(buffer == "START_ROOM")
-        {
-            input >> buffer; 
-            currentRoom = rooms[buffer]; 
-        }
-        else if(buffer == "END_ROOM")
-        {
-            input >> endRoom; 
-        }
-    }
-}
-
-void Game::createRoom(string id, string name, string description, string north, string east, string south, string west)
-{
-    rooms[id]= new Room(id, name, description); 
-    rooms[id]-> set
-}
--- Room.hpp
+++ Room.hpp
@@ -1,5 +1,5 @@
-#ifndef ROOM_H
-#define ROOM_H
+#ifndef ROOM_HPP
+#define ROOM_HPP
 #endif
 
 #include <iostream>
@@ -10,7 +10,6 @@ class Room
 {
     public: 
         Room(); 
-        Room(string id, string name, string description); 
 
     private: 
         string id; 
@@ -21,23 +20,7 @@ class Room
 
 Room::Room()
 {
-    id = ""; 
-    name = ""; 
-    description = ""; 
 
-    for(int i = 0; int i<4; i++)
-    {
-        string neighbour[i] = "NULL"
-    }
 }
 
-Room::Room(string id, string name, string description)
-{
-    this->id = id; 
-    this-> name = name; 
-    this->description = description; 
-//    neighbour[0] = north; 
-    // neighbour[1] = east; 
-    // neighbour[2] = south; 
-    // neighbour[3] = west; 
-}
+
