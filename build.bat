e86bbc50ecb8dd7e019e24dd436be5d53bff4656
LaPlatti <38055640+LaPlatti@users.noreply.github.com>
2020-11-21 16:06:49 +0100

    added scripts for execution

---
+++ build.bat
@@ -0,0 +1,9 @@
+@echo off
+title Adventure Game 
+cls
+echo Das Programm wird jetzt kompiliert ... 
+g++ -g source/main.cpp source/Assessment.cpp source/Auswertung.cpp source/BeginnSemester.cpp source/Betriebssysteme.cpp source/Chef.cpp source/Englisch.cpp source/Game.cpp source/Informatik.cpp source/Mathe.cpp source/Programmieren.cpp source/Stage.cpp -o adventure.exe
+echo Kompilierung abgeschlossen.
+echo Druecke eine beliebige Taste um das Programm zu starten!
+pause >nul
+adventure.exe
